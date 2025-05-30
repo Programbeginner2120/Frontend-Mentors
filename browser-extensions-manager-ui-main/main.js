9b4f09e89d164aeb9945bc115e8f42eac9df489e
Matthew Killeen <Matthewkilleen99@gmail.com>
2025-05-30 12:59:42 -0400

    refactored code to make a bit more maintainable

--- browser-extensions-manager-ui-main/main.js
+++ browser-extensions-manager-ui-main/main.js
@@ -32,28 +32,39 @@ function _initializeEventListeners() {
 function _initializeColorStateButtonEventListener() {
     try {
         const pageContainer = document.querySelector('.page-container');
-        const mainToolbar = document.querySelector('.main-toolbar');
         const pageColorStateButtonContainer = document.querySelector('.main-toolbar-color-state-button-container');
-        const pageColorStateButton = document.querySelector('.main-toolbar-color-state-button');
         document.addEventListener('click', (event) => {
             if (!pageColorStateButtonContainer.contains(event.target)) return;
 
             if (pageColorState === ColorState.LightMode) {
                 pageColorState = ColorState.DarkMode;
-                pageContainer.classList.add('page-container-dark-mode');
-                mainToolbar.classList.add('main-toolbar-dark-mode');
-                pageColorStateButtonContainer.classList.add('main-toolbar-color-state-button-container-dark-mode');
-                pageColorStateButton.style.backgroundImage = "url('./assets/images/icon-moon.svg')";
+                _toggleDarkModeToggleableElements(true);
             } else if (pageColorState === ColorState.DarkMode) {
                 pageColorState = ColorState.LightMode;
-                pageContainer.classList.remove('page-container-dark-mode');
-                mainToolbar.classList.remove('main-toolbar-dark-mode');
-                pageColorStateButtonContainer.classList.remove('main-toolbar-color-state-button-container-dark-mode');
-                pageColorStateButton.style.backgroundImage = "url('./assets/images/icon-sun.svg')";
+                _toggleDarkModeToggleableElements(false);
             }
         });
     } catch (error) {
         console.error("Error initializing color state button event listener: ", error);
+    }
+}
+
+function _toggleDarkModeToggleableElements(toggleToDarkMode) {
+    const pageContainer = document.querySelector('.page-container');
+    const mainToolbar = document.querySelector('.main-toolbar');
+    const pageColorStateButtonContainer = document.querySelector('.main-toolbar-color-state-button-container');
+    const pageColorStateButton = document.querySelector('.main-toolbar-color-state-button');
+
+    if (toggleToDarkMode) {
+        pageContainer.classList.add('page-container-dark-mode');
+        mainToolbar.classList.add('main-toolbar-dark-mode');
+        pageColorStateButtonContainer.classList.add('main-toolbar-color-state-button-container-dark-mode');
+        pageColorStateButton.style.backgroundImage = "url('./assets/images/icon-moon.svg')";
+    } else {
+        pageContainer.classList.remove('page-container-dark-mode');
+        mainToolbar.classList.remove('main-toolbar-dark-mode');
+        pageColorStateButtonContainer.classList.remove('main-toolbar-color-state-button-container-dark-mode');
+        pageColorStateButton.style.backgroundImage = "url('./assets/images/icon-sun.svg')";
     }
 }
 
